ff9cfcb08d409f1552b439fd359f03bdbed5f62c
Ben Youngblood <bx.youngblood@gmail.com>
2012-08-21 14:01:29 -0700

    initial schema

---
+++ data/schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS `contact` (
+  `contact_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `display_name` varchar(255) DEFAULT NULL,
+  `parent_contact_id` int(11) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`contact_id`),
+  KEY `parent_contact_id` (`parent_contact_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `contact_addresses` (
+  `contact_id` int(11) unsigned NOT NULL,
+  `tag` varchar(255) DEFAULT NULL,
+  `address_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`contact_id`,`address_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `contact_email` (
+  `contact_id` int(11) unsigned NOT NULL,
+  `tag` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`contact_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `contact_phone` (
+  `contact_id` int(11) unsigned NOT NULL,
+  `tag` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  PRIMARY KEY (`contact_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `contact_url` (
+  `contact_id` int(11) unsigned NOT NULL,
+  `tag` varchar(255) DEFAULT NULL,
+  `url` text NOT NULL,
+  PRIMARY KEY (`contact_id`)
+) ENGINE=InnoDB;
